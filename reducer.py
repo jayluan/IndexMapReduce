bd061b28f7face970e56c9e4eb82c15142a9a156
Jay Luan <jayluan7@gmail.com>
2014-02-08 20:53:18 -0800

    added new reducer_pt2.py to calculate TF-IDF and also reformated reducer.py

--- reducer.py
+++ reducer.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
+'''
+    reducer.py
+    purpose: does the reduction coming from the mapper
+    input:
+    (word) (freq) (url) (doc#) (#words_in_doc) (pos1) (pos2) (pos3) ...
 
+    output:
+    (word) (freq) (# docs w/ word) (doc#1) (#words_total) (#of_given_word) (pos1) (pos2) (pos3) ... (doc2) (#words_total) (#of_given_word) (pos1) (pos2) ...
+
+
+'''
 from operator import itemgetter
 import sys
 
@@ -7,11 +17,12 @@ current_word = None
 current_count = 0
 word = None
 ref_list = {}
+DEBUG = False
 
 # input comes from STDIN
-#f = open('test.txt')
-#for line in f:
-for line in sys.stdin:
+f = open('../test.txt')
+for line in f:
+#for line in sys.stdin:
 
     # remove leading and trailing whitespace
     line = line.strip()
@@ -19,7 +30,15 @@ for line in sys.stdin:
         continue
 
     # parse the input we got from mapper.py
-    word, count, url, index = line.split('\t', 3)
+    positions = []
+    entry = line.split('\t')
+    word = entry[0]
+    count = entry[1]
+    url = entry[2]
+    index = entry[3]
+    numWords = entry[4]    # total # of words in document
+    if len(entry) > 5:
+        positions = entry[5:]
 
     # convert count (currently a string) to int
     try:
@@ -41,22 +60,37 @@ for line in sys.stdin:
     # by key (here: word) before it is passed to the reducer
     if current_word == word:
         current_count += count
-        ref_list[index] = count
+        ref_list[index] = [count, numWords, positions]
     else:
         if current_word:
             # write result to STDOUT
-            printLine = '%s\t%s\t' % (current_word, current_count)
+            printLine = '%s\t%s\t%d\t' % (current_word, current_count, len(ref_list))
             for i in ref_list:
-                printLine += "(%s\t%s)\t" % (i, ref_list[i])
+                if DEBUG:
+                    printLine += "("
+                printLine += "%d\t%s\t%d" % (i, ref_list[i][1], ref_list[i][0])    # Doc_num, numWords, count
+                for k in ref_list[i][2]:
+                    printLine += "\t%s" % (k)
+                if DEBUG:
+                    printLine += ")"
+                printLine += "\t"
+                
             print printLine
         current_count = count
         current_word = word
         ref_list = {}
-        ref_list[index] = count
+        ref_list[index] = [count, numWords, positions]
 
 # do not forget to output the last word if needed!
 if current_word == word:
-    printLine = '%s\t%s\t' % (current_word, current_count)
+    printLine = '%s\t%s\t%d\t' % (current_word, current_count, len(ref_list))
     for i in ref_list:
-        printLine += "(%s\t%s)\t" % (i, ref_list[i])
+        if DEBUG:
+            printLine += "("
+        printLine += "%d\t%s\t%d" % (i, ref_list[i][1], ref_list[i][0])
+        for k in ref_list[i][2]:
+            printLine += "\t%s" % (k)
+        if DEBUG:
+            printLine += ")"
+        printLine += "\t"
     print printLine
---
+++ reducer_pt2.py
@@ -0,0 +1,59 @@
+'''
+    reducer_pt2.py
+    purpose: takes pipe from reducer and computes the TF-IDF index
+    the layout of the input is as follows:
+
+    (word) (total_freq) (#of_documents) (doc#1) (#words_total) (freq) (pos1) (pos2) (pos3) ... (doc#2) (#words_total) (freq) (pos1) (pos2) ...
+
+    the output is:
+   (word) (total_freq) (#of_documents) (doc#1) (TFIDF) (#words_total) (freq) (pos1) (pos2) (pos3) ... (doc#2) (TFIDF) (#words_total) (freq) (pos1) (pos2) ...
+
+
+'''
+
+import sys
+import math
+
+stored_data = []
+stored_doc_count_word = {}    # number of documents with a given word
+#f = open('../test2.txt')
+#for line in f:
+for line in sys.stdin:
+    #count the number of lines and get # of docs w/ the term in it
+    line = line.strip()
+    if line.strip() == '':
+        continue
+
+    # parse the input we got from mapper.py and store it
+    entry = line.split('\t')
+    stored_data.append(entry)
+
+#loop through all the stored input data
+for entry in stored_data:
+    tfidf = {}
+    word = entry[0]
+    count = entry[1]
+    numDocs = entry[2]    # number of documents with this word in it
+    i = 3
+
+    #loop through the rest of the doc and compute all the TFIDF's
+    while i is not len(entry):
+        doc = entry[i]
+        i += 1
+        totalWords = entry[i]
+        tfidf_index = i    # we want to store tfidf right before frequency, so we keep track of the index of that position
+        i += 1
+        freq = entry[i]
+        i += int(freq)+1
+
+        #calculate tfidf for the current word for this specific document
+        tf = float(freq)/float(totalWords)
+        idf = math.log(float(len(stored_data))/float(numDocs))
+        tfidf[tfidf_index] = tf*idf
+
+    #insert tfidf values into the entry
+    for i in tfidf:
+        entry.insert(i, str(tfidf[i]))
+
+    #print the line
+    print "\t".join(entry)
